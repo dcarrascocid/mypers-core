f3743b25b1e60519bb7df41c191cf14c7870e559
dcarrascocid <dcarrascocid@gmail.com>
2020-07-13 23:18:57 -0400

    coneccion a la base de datos

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-//Requieres
+const  dotenv = require  ('dotenv');
+dotenv.config();
 var express = require ('express');
+const { Pool } = require('pg') 
+const pool = new Pool()
+//Requieres
+
+
 
 // INICIALIZAR VARIABLES
 
 var app =express();
+//CONEXIONA A LA BASE DE DATOS
+pool.connect((err, client, release) => {
+    if (err) {
+      return console.error('Error acquiring client', err.stack)
+    }
+    client.query('SELECT NOW()', (err, result) => {
+      release()
+      if (err) {
+        return console.error('Error executing query', err.stack)
+      }
+      console.log(result.rows)
+    })
+  })
+
 
 //RUTAS
 app.get('/', (req, res, next) =>{
@@ -12,11 +32,11 @@ res.status(200).json({
     ok:true,
     mensaje:'petición Realizada correctamente'
 })
-});
+}); 
 
 
 // ESCUCHAR PETICIONES
 
 app.listen(3000,() => {
-    console.log('Express Server puerto 3000:,\x1b[32m%s\x1b[0','Online');
+    console.log('Server puerto 3000:\x1b[32m%s\x1b[0','Online');
 })
